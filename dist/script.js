575709efc37a916755cf696b868f338cc1d12ae4
A-007481D <labidabdelmalek@gmail.com>
2024-11-01 11:29:22 +0100

    adding tasks via modal now

--- dist/script.js
+++ dist/script.js
@@ -4,6 +4,12 @@ const openModalButton = document.getElementById('openModal');
 const modalOverlay = document.getElementById('modalOverlay'); 
 const closeModalButton = document.getElementById('closeModal'); 
 
+let taskTitle = document.getElementById('taskTitle');
+let taskStatus = document.getElementById('taskStatus');
+let taskPriority = document.getElementById('taskPriority');
+let taskDeadline = document.getElementById('TaskDeadline');
+let taskDescription = document.getElementById('taskDescription');
+
 openModalButton.addEventListener('click', () => {
     modalOverlay.classList.remove('hidden'); 
 });
@@ -15,12 +21,70 @@ closeModalButton.addEventListener('click', () => {
 
 // add task
 
-let array =[''];
+let taskArray =[];
 
 
-function appen1() {
+
+const createTask = document.getElementById("createTask");
+createTask.onclick = function () {
+    const task = {
+        id: Date.now(),
+        title : taskTitle.value,
+        description: taskDescription.value,
+        status: taskStatus.value,
+        deadline: taskDeadline.value,
+        priority: taskPriority.value,
+
+    }
+
+    taskArray.push(task);
+    render();
+};
+
+function render(){
+    const todoTasksContainer = document.getElementById('todoTasks');
+    const inProgressContainer = document.getElementById('inProgressTasks');
+    const doneTasksContainer = document.getElementById('doneTasks');
+
+    todoTasksContainer.innerHTML="";
+    inProgressContainer.innerHTML="";
+    doneTasksContainer.innerHTML="";
+
+    taskArray.forEach(task => {
+        const div = document.createElement("div")
+        div.classList.add("bg-green-100", "rounded-lg", "p-4", "shadow", "cursor-pointer")
+        div.draggable = "true"
+
+        div.innerHTML =`
+                <h3 class="font-medium text-green-800">${task.title}</h3>
+                <p class="text-sm text-green-700">${task.description}</p>
+                <div class="flex justify-between space-x-2 mt-3">
+                  <button
+                    class="bg-yellow-500 rounded-2xl w-11 ">${task.priority}
+                  </button>
+                  <div>
+                    <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-1.5 px-3 rounded">Edit</button>
+                    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-1.5 px-3 rounded">Delete</button>
+                  </div>
+                </div>            
+        `
+        todoTasksContainer.appendChild(div)
+    });
 
 }
+
+// to empty the form after submit
+// document.getElementById('taskTitle').value = '';
+// document.getElementById('taskStatus').value = '';
+// document.getElementById('taskPriority').value = '';
+// document.getElementById('TaskDeadline').value = '';
+// document.getElementById('taskDescription').value = '';
+
+
+
+
+
+
 
 
 
